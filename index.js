1e3846fde135f3045139eb0573db4ab0ae7c1003
adityabastyas <adityabastyasmulya@gmail.com>
2025-11-27 01:33:25 +0700

    feat: add hamburger menu

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const inputPassword = document.querySelector('input[name="password"]');
 const toggle = document.querySelector("#toggle");
 const icon = document.querySelector(".icon");
+const icon2 = document.querySelector(".icon2");
 
 toggle.addEventListener("click", () => {
   const isHidden = inputPassword.type === "password";
   if (isHidden) {
     inputPassword.type = "text";
-    icon.style.fill = "red";
+    icon.style.display = "none";
+    icon2.style.display = "block";
   } else {
     inputPassword.type = "password";
-    icon.style.fill = "black";
+    icon.style.display = "block";
+    icon2.style.display = "none";
+  }
+});
+
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#navMenu");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+
+  if (navMenu.classList.contains("show")) {
+    hamburger.textContent = "✕";
+  } else {
+    hamburger.textContent = "☰";
   }
 });
